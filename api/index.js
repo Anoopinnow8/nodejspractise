0ee91cd940275410e7a3d0a085b472d55e83f90c
Anoop <apatel@innow8apps.com>
2024-11-18 09:59:54 +0530

    add function

--- api/index.js
+++ api/index.js
@@ -1,3 +1,18 @@
 module.exports = (req, res) => {
-  res.status(200).send('Hello, World!');
+  const { method, url } = req;
+  const { name } = req.query;
+
+  if (url === "/") {
+    res.status(200).send("Welcome to the Home Page!");
+  } else if (url.startsWith("/greet")) {
+    if (name) {
+      res.status(200).send(`Hello, ${name}!`);
+    } else {
+      res.status(400).send("Please provide a name in the query, e.g., /greet?name=YourName");
+    }
+  } else if (url.startsWith("/about")) {
+    res.status(200).send("This is the About Page.");
+  } else {
+    res.status(404).send("Page not found.");
+  }
 };
